a92567441e041e9e889ee03daf352a132e752850
Ethan Hann <ethanhann@gmail.com>
2024-11-30 14:51:51 -0500

    Update extractor

--- axum-login/src/extract.rs
+++ axum-login/src/extract.rs
@@ -1,3 +1,4 @@
+use std::future::{ready, Future};
 use async_trait::async_trait;
 use axum::{
     extract::FromRequestParts,
@@ -14,10 +15,13 @@ where
 {
     type Rejection = (StatusCode, &'static str);
 
-    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
-        parts.extensions.get::<AuthSession<_>>().cloned().ok_or((
+    fn from_request_parts(
+        parts: &mut Parts,
+        _state: &S,
+    ) -> impl Future<Output = Result<Self, Self::Rejection>> + Send {
+        ready(parts.extensions.get::<AuthSession<_>>().cloned().ok_or((
             StatusCode::INTERNAL_SERVER_ERROR,
             "Can't extract auth session. Is `AuthManagerLayer` enabled?",
-        ))
+        )))
     }
 }
